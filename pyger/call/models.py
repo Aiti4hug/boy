39c4946d628bf7cbe85dd1ab375ab3fbbd9105ec
Aiti4hug <8691889nko@gmail.com>
2024-11-21 11:23:46 +0600

    pritty boy

---
+++ pyger/call/models.py
@@ -0,0 +1,24 @@
+from django.contrib.gis.shortcuts import render_to_kml
+from django.db import models
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    have = models.BooleanField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    video = models.FileField(upload_to='videos/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product_name} - {self.created_date}'
+
---
+++ pyger/call/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'product_name', 'description', 'price', 'have', 'image', 'video', 'created_date', 'category']
